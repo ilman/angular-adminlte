6dcf5363295306f923b967a9b8a19eb6c5737ed5
Ilman Maulana <ilman.maulana@gmail.com>
2016-04-20 02:18:23 +0700

    Add basic angular config

---
+++ app/config.js
@@ -0,0 +1,11 @@
+var adminApp = angular.module('adminApp', ['ui.router']);
+
+adminApp.directive('includeReplace', function () {
+    return {
+        require: 'ngInclude',
+        restrict: 'A', /* optional */
+        link: function (scope, el, attrs) {
+            el.replaceWith(el.children());
+        }
+    };
+});
